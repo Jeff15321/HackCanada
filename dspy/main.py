40148512fa30abc6847b1872177ee8d1994ef014
alvina-yang <alvinay73@gmail.com>
2024-11-16 23:30:39 -0500

    ...

--- dspy/main.py
+++ dspy/main.py
@@ -1,10 +1,4 @@
 from model import *
-from prompt import *
-
-user_role = "Biomass Power Plant Managers"
-user_task = "Prepare and manage biomass plant budgets."
-
-model = "gpt-4o-mini"
 
 class BiomassBudgetPipeline(dspy.biomass_budget):
     def __init__(self):
@@ -31,13 +25,6 @@ class BiomassBudgetPipeline(dspy.biomass_budget):
         }
 
 if __name__ == "__main__":
-
-    initial_prompt = initialize_initial_task_prompt(user_role, user_task)
-    print(initial_prompt)
-
-    print("*"*50)
-    print("*"*50)
-    print("*"*50)
 
     resp = run_api(model, initial_prompt)
     print(resp)
