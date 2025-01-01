4ee857d472ab3f1024be6c64457f57ca53e232f7
Julien <julienliang2740@gmail.com>
2024-12-31 22:22:18 -0500

    minor edits

--- multiagent/version2/main.py
+++ multiagent/version2/main.py
@@ -3,10 +3,14 @@ from agent_system import *
 
 
 list_of_agents = ["Task Determinator Agent", "Prompt Generator Agent", "Process Creator Agent", "Search Agent", "Title Context Agent", "Task Context Agent", "Clarity Agent", "Relevance Agent", "Precision Agent", "Brevity Agent", "Consolidator Agent", "Completeness Agent"]
-list_of_file_paths = ["./IA_ZhuDi.pdf"]
+# list_of_file_paths = ["./IA_ZhuDi.pdf"]
+list_of_file_paths = []
 
-task = "Examine documents to determine degree of risk from factors such as applicant health, financial standing and value, and condition of property."
-role = "Insurance Underwriters"
+# task = "Examine documents to determine degree of risk from factors such as applicant health, financial standing and value, and condition of property."
+# role = "Insurance Underwriters"
+
+task = "Come up with a list of essential institutions and structures that a typical kingdom from the Bronze Age would have"
+role = "Historical Country Maker"
 
 rounds = 2
 model = "gpt-4o-mini"
